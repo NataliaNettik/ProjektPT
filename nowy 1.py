877526af8b3e3d33b2257e034ebd7082328fd8fc
NataliaNettik <155383294+NataliaNettik@users.noreply.github.com>
2024-01-28 20:42:34 +0100

    Update nowy 1.py

--- nowy 1.py
+++ nowy 1.py
@@ -24,6 +24,14 @@ def w_trakcie_zadanie():
     if selected_index:
         listbox_zadania.itemconfig(selected_index, {'bg': '#a6e2ff'})
         aktualizuj_pasek_postepu()
+# Funkcja zmieniająca kolor tła zaznaczonego zadania
+def wykonaj_zadanie():
+    selected_index = listbox_zadania.curselection()  # Pobranie indeksu zaznaczonego zadania
+    if selected_index:
+        current_bg = listbox_zadania.itemcget(selected_index, "bg")  # Pobranie aktualnego koloru tła zadania
+        new_bg = '#61a0ff' if current_bg != '#61a0ff' else ''  # Zmiana koloru tła na niebieski, jeśli niebieski nie był wcześniej ustawiony, w przeciwnym razie usunięcie koloru
+        listbox_zadania.itemconfig(selected_index, {'bg': new_bg})  # Ustawienie nowego koloru tła zadania
+        aktualizuj_pasek_postepu()  # Aktualizacja paska postępu po zmianie koloru zadania
 
 # Napis nad głównym obszarem z zadaniami
 label_lista_zadan = Label(root, text='L I S T A   Z A D A Ń', font=('Helvetica', 16, 'bold'), bg='#034078', fg='white')
